72315af00509f552d3a16b139737c6244cd945e5
William Alfonso <willfonso@gmail.com>
2022-04-07 22:10:41 -0300

    MASTER

---
+++ funciones.js
@@ -0,0 +1,13 @@
+class producto {
+constructor (nombre, categoria, precio, stock) {
+    this.nombre = nombre.toUpperCase ();
+    this.categoria = categoria.toUpperCase();
+    this.precio = parseFloat(precio);
+    this.stock = parseInt(stock);
+
+}
+descuento(){
+    if pr
+}
+
+}
